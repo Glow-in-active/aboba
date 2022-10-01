cc2709c0768dcb0ba3dbeed1b60d9b8c65864f0d
tum4uk5 <timurtimurcik7@gmail.com>
2022-10-01 14:34:51 +0300

    creating simple interface and realization searh by phonenumer, email, name

--- find.py
+++ find.py
@@ -26,3 +26,39 @@ print("Contacts loading started")
 for i in f:
     Database.append(Contact(i))
 print("Contacts loading finished")
+while True:
+    print("Select a command")
+    print("1 - Search by phone number")
+    print("2 - Search by email")
+    print("3 - Search by name")
+    print("4 - Search for all contacts that you do not have detected email or phone")
+    print("5 - Search for all contacts that you do not have detected email and phone")
+    print("6 - Edit contact")
+    command = int(input())
+    if command == 1:
+        number = input("Enter phone number ")
+        for i in Database:
+            if i.phonenumber == number:
+                print(i.surname, i.name, i.patronymic, i.phonenumber, i.email)
+        print()
+    if command == 2:
+        email = input("Enter email ")
+        for i in Database:
+            if i.email == email:
+                print(i.surname, i.name, i.patronymic, i.phonenumber, i.email)
+        print()
+    if command == 3:
+        name = input("Enter name ").strip().split()
+        for i in Database:
+            if len(name) > 0:
+                if name[0]==i.name or name[0]==i.surname or name[0]==i.patronymic:
+                    if len(name)==1:
+                        print(i.surname, i.name, i.patronymic, i.phonenumber, i.email)
+                    else:
+                        if name[1]==i.name or name[1]==i.surname or name[1]==i.patronymic:
+                            if len(name)==2:
+                                print(i.surname, i.name, i.patronymic, i.phonenumber, i.email)
+                            else:
+                                if name[1]==i.name or name[1]==i.surname or name[1]==i.patronymic:
+                                    print(i.surname, i.name, i.patronymic, i.phonenumber, i.email)
+        print()
